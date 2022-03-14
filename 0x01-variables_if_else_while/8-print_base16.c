a407557193e7bce5cb6a1839a1e19562fee91ef2
root <isaacibezim1998@gmail.com>
2022-03-13 20:15:11 -0700

    print hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prnt hexadecimal numbers
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	for (n = 'A'; n <= 'F'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
